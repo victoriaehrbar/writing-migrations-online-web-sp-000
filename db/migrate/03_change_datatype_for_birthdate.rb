36d05f128dc4fae78268dd8cb3364ec0df1cb51b
victoriaehrbar <victoriaehrbar@gmail.com>
2021-02-03 23:02:16 +0000

    Automatically backed up by Learn

--- db/migrate/03_change_datatype_for_birthdate.rb
+++ db/migrate/03_change_datatype_for_birthdate.rb
@@ -1,5 +1,5 @@
 class ChangeDatatypeForBirthdate < ActiveRecord::Migration[5.1]
   def change
-    change_column :students, :birthdate, :datetime
+    change_column :table_name, :column_name, :type
   end
 end
